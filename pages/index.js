26b902c26d6c63841896374449859eb68df525ee
Nggohel <gohelnidhi15898@gmail.com>
2023-12-25 21:39:04 +0530

    add Home

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
-import { useState, useCallback } from 'react';
-import ItemList from '../components/ItemList';
+import { useState, useCallback } from "react";
+import ItemList from "../components/ItemList";
 
 export default function Home() {
-    const [items, setItems] = useState([]);
-    const [message, setMessage] = useState('');
+  const [items, setItems] = useState([]);
+  const [message, setMessage] = useState("");
 
-    
+  const addItem = useCallback(() => {
+    const newItem = `Item ${items.length + 1}`;
+    setItems((prevItems) => [...prevItems, newItem]);
+    setMessage("Item added successfully!");
 
-    return (
-        <div>
-            <button >Add Item</button>
-            <ItemList items={items} />
-            {message && <p id='message'>{message}</p>}
-        </div>
-    );
+    // Clear the message after 3 seconds
+    setTimeout(() => {
+      setMessage("");
+    }, 3000);
+  }, [items]);
+
+  return (
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      <ItemList items={items} />
+      {message && <p id="message">{message}</p>}
+    </div>
+  );
 }
